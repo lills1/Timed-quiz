3f25cce7a911f078d2ca9ef82c5882c0cd439d56
lills1 <lilly.stephenson4@gmail.com>
2021-09-14 15:29:28 +1000

    added in a hide/show for function startquiz, did a couple of drafts that i commented out for timer

--- script.js
+++ script.js
@@ -5,46 +5,50 @@ var answer1El = document.getElementById("answer1");
 var answer2El = document.getElementById("answer2");
 var answer3El = document.getElementById("answer3");
 var answer4El = document.getElementById("answer4");
-
+var timerEl= document.getElementsByClassName("timer-right");
 var startQuizBtn = document.getElementById("generate");
 
 startQuizBtn.onclick = startQuiz;
 
 function startQuiz() {
     document.getElementById("homepage").style.display = "none";
+    document.getElementById("generate").style.display = "none"; 
+    document.getElementById("hidden-question").style.display = "block";
 }
 
-
-var questions = [
-  {
-    question: "what does HTML stand for?",
-    answers: ["Hypertext markup language", "hypertype market language", "hypertext markup linguistics"],
-    correctAnswer: 0,
-  },
-  {
-    question: "what is the purpose of a function in javascript?",
-    answers: ["it stores multiple values in a single variable", "it allows you to define a block of code to execute as many times as wanted", "They check specific conditions"],
-    correctAnswer: 1,
-  },
-  {
-    question: "When targetting a class in css, which of the following character is used:",
-    answers: ["#", "", "."],
-    correctAnswer: 2,
-  },
-  {
-    question: "In Javascript, which of the following out of the array would be conisdered [2]: var animals =[ 'turtles', 'cats', 'owl', 'donkey']",
-    answers: ["turtles", "cats", "owl", "donkey"],
-    correctAnswer: 3, 
-  }
-];
-
-var currentQuestionIndex = 0;
+ 
+// document.getElementByid("generate").addEventListener("click", function() {
+// var timeleft = 90;
+// var downloadTimer = setInterval(function(){
+//   if(timeleft <= 0){
+//     clearInterval(downloadTimer);
+//     document.getElementById("timer-right").textContent = "Finished";
+//   } else {
+//     document.getElementById("timer-right").textContent = timeleft + " seconds remaining";
+//   }
+//   timeleft -= 1;
+// }, 90000);
+// });
+// function timer(){
+//   var timerInterval=SetInterval(function() {
+//     timerCount--;
+//     console.log(timerCount);
+//     timerEl.textContent=timerCount + "seconds remaining";
+//     if (timerCount===1) {
+//       timerEl.textContent = timerCount + "second remaining";
+//   } else if (timerCount===0){
+//     clearInterval(timer);
+//     timerEl.textContent = timerCount + "You've run out of time!";
+//   }
+// }, 1000)
+// }
+// updateQuestion();
 
 function updateQuestion() {
   var currentQuestion = questions[currentQuestionIndex];
 
   if (!currentQuestion) {
-    console.log('quiz done')
+    document.write("quiz done");
     return;
   }
 
@@ -69,31 +73,40 @@ function checkAnswer(clickedAnswer) {
   }
   
   if (currentQuestion.correctAnswer === clickedAnswer) {
-    console.log('correct answer');
+    console.log("correct answer!");
   }
   else {    
-    console.log('wrong answer');
+    console.log("wrong answer");
   }
 
   currentQuestionIndex++;
   updateQuestion();
 }
 
-updateQuestion();
 
+var questions = [
+  {
+    question: "what does HTML stand for?",
+    answers: ["Hypertext markup language", "Hypertype market language", "Hypertext markup linguistics"],
+    correctAnswer: 0,
+  },
+  {
+    question: "What is the purpose of a function in javascript?",
+    answers: ["it stores multiple values in a single variable", "it allows you to define a block of code to execute as many times as wanted", "They check specific conditions"],
+    correctAnswer: 1,
+  },
+  {
+    question: "When targetting a class in css, which of the following character is used:",
+    answers: ["#", "", "."],
+    correctAnswer: 2,
+  },
+  {
+    question: "In Javascript, which of the following out of the array would be conisdered [2]: var animals =[ 'turtles', 'cats', 'owl', 'donkey']",
+    answers: ["turtles", "cats", "owl", "donkey"],
+    correctAnswer: 3, 
+  }
+];
 
+var currentQuestionIndex = 0;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+//to do: add in timer function, add in javascript to comment on if you get right/wrong answer
