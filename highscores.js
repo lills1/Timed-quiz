59aa28e707928dae355ecae0520bfadee510d5de
lills1 <lilly.stephenson4@gmail.com>
2021-09-17 15:57:14 +1000

    similar to html, modified code

--- highscores.js
+++ highscores.js
@@ -1,21 +1,29 @@
+var highScoreEl= document.getElementById("highScoresContainer");
+var highscores="";
 var highScoreList = JSON.parse(localStorage.getItem('highscores')) || [];
 if (localStorage.getItem('highscores') !== null) {
     highScoreList = JSON.parse(localStorage.getItem('highscores'));
   }
-  highScore = {
+  /*highScore = {
     name: currentName.value,
     score: gameScore
   }
   highScoreList.push(highscore);
   window.localStorage.setItem('highscores', JSON.stringify(highScoreList));
-  
-
-  highScoreText.innerHTML= "<tr><td>Name -</td><td> Score</td></tr><br>";
+  */
+  text = "<h3>High Scores</h3>\n<table><tr><td>Name -</td><td> Score</td></tr>";
+  for (let i = 0; i < highScoreList.length; i++) {
+    text += "<tr><td>"+highScoreList[i].name+"</td><td>"+highScoreList[i].score+ "</td></tr>";
+  }
+  text += "</table>";
+  highScoreText.innerHTML= text;
     //show highscores window
     if (localStorage.getItem('highscores')) {
-        highScoreText.innerHTML += highScoreList
+        // highScoreText.innerHTML += highScoreList
     } else {
-        highScoreText.textContent += "No high scores to display :("
+        highScoreText.innerHTMLContent += "No high scores to display"
     }
     highScoresContainer.style.display = "block"; 
+
+
   
